a78aef98ad6064654dd617a4f045bf3f50be73c0
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-08 03:58:13 +0100

    0x01-lockboxes/

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -31,13 +31,21 @@ def canUnlockAll(boxes):
             # mark the index of the box that didnt open
             Box.append(i)
     # settlethe unlocked boxes
-    for j in range(len(Box)):
+    lel = len(Box)
+    for j in range(lel):
         if len(Box) == 0:
             break
         if Box[0] in list:
             for ele in boxes[Box[0]]:
                 list.append(ele)
             Box.pop(0)
+    for k in range(len(Box) - 1, -1, -1):
+        if len(Box) == 0:
+            break
+        if Box[len(Box) - 1] in list:
+            for ele in boxes[Box[len(Box) - 1]]:
+                list.append(ele)
+            Box.pop(len(Box) - 1)
     if len(Box) > 0:
         return False
     else:
