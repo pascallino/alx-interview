3fcaafd9e935eed9df9030f8a0831724921ff7fd
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-23 08:23:48 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,8 +5,8 @@ import re
 
 # Initialize hashtable
 hashtable = {
-             200: 0, 301: 0, 400: 0,
-             401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+             '200': 0, '301': 0, '400': 0,
+             '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 counter = 0
 total_size = 0
 
@@ -16,7 +16,7 @@ try:
         line_list = line.split(" ")
 
         if len(line_list) > 4:
-            status_code = int(line_list[-2])
+            status_code = line_list[-2]
             file_size = int(line_list[-1])
 
             # Increment status code count
@@ -36,7 +36,6 @@ try:
                         print('{}: {}'.format(key, value))
 
                 # Reset counters and hashtable for the next 10 lines
-                
 
 except Exception as err:
     pass
