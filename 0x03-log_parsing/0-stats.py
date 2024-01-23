a07af24a9b4893cc8f7b627e9f65988125d24129
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-23 08:00:51 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
 import sys
 import re
 
-
 # Initialize hashtable
 hashtable = {"file_size": 0, "counter": 0,
              200: 0, 301: 0, 400: 0,
@@ -16,31 +15,32 @@ try:
         if match:
             status_code = int(match.group(1))
             file_size = int(match.group(2))
+
             # Increment status code count
             if status_code in hashtable:
                 hashtable[status_code] += 1
             else:
                 hashtable[status_code] = 1
 
-                # Accumulate total file size
+            # Accumulate total file size
             hashtable["file_size"] += file_size
             hashtable['counter'] += 1
 
             # Check if it's time to print statistics
             if hashtable['counter'] == 10:
-                print(f'File size: {hashtable["file_size"]}')
+                print('File size: {}'.format(hashtable["file_size"]))
                 for s in [200, 301, 400, 401, 403, 404, 405, 500]:
                     if s in hashtable and hashtable[s] > 0:
-                        print(f'{s}: {hashtable[s]}')
+                        print('{}: {}'.format(s, hashtable[s]))
 
                 # Reset counters and hashtable for the next 10 lines
                 hashtable['counter'] = 0
 
 except KeyboardInterrupt:
     # Handle Ctrl+C interruption, print current statistics, and exit
-    print(f'File size: {hashtable["file_size"]}')
+    print('File size: {}'.format(hashtable["file_size"]))
     for status in [200, 301, 400, 401, 403, 404, 405, 500]:
         if status in hashtable and hashtable[status] > 0:
-            print(f'{status}: {hashtable[status]}')
+            print('{}: {}'.format(status, hashtable[status]))
 
     sys.exit(0)
